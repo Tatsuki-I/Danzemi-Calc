0a5378a67e51b95265b95b636fccfbe5841efea9
Tatsuki-I <tatsuki.devel@gmail.com>
2019-06-03 12:32:52 +0900

    update

--- Haskell/calc/src/ParsecTest.hs
+++ Haskell/calc/src/ParsecTest.hs
@@ -4,14 +4,14 @@ module ParsecTest where
 
 import Data.Char
 import Control.Applicative hiding (many)
-import Text.Parsec
-import Text.Parsec.Char
+import Text.Parsec hiding ((<|>))
 
 num :: Stream s m Char => ParsecT s u m Int
 num =  digitToInt <$> digit
 
 op :: (Num a, Stream s m Char) => ParsecT s u m (a -> a -> a)
-op =  const (+) <$> char '+'
+op =  (const (+) <$> char '+') <|>
+      (const (-) <$> char '-')
 
 expr :: Stream s m Char => ParsecT s u m Int
 expr =  num `chainl1` op
