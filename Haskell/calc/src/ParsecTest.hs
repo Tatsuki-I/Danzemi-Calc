da7bc8891337e38ce1aa3e13eb1ab3e49dabb277
Tatsuki-I <tatsuki.devel@gmail.com>
2019-06-03 12:50:07 +0900

    update

--- Haskell/calc/src/ParsecTest.hs
+++ Haskell/calc/src/ParsecTest.hs
@@ -6,18 +6,30 @@ import Data.Char
 import Control.Applicative hiding (many)
 import Text.Parsec hiding ((<|>))
 
-num :: Stream s m Char => ParsecT s u m Int
-num =  do xs <- many $ digitToInt <$> digit
-          return $ foldl f 0 xs
-          where f x y = x * 10 + y
+digitToDouble :: Char -> Double
+digitToDouble =  fromIntegral . digitToInt
 
---num =  digitToInt <$> digit
+symbol    :: Stream s m Char => String -> ParsecT s u m String
+symbol xs =  do result <- string xs
+                spaces
+                return result
+
+num :: Stream s m Char => ParsecT s u m Double
+num =  do xs <- many $ digitToDouble <$> digit
+          dot <- optionMaybe (char '.')
+          ys <- many $ digitToDouble <$> digit
+
+          spaces
+
+          return $ foldl f 0 xs + foldl g 0 ys
+          where f x y = x * 10 + y
+                g x y = x + y * 0.1
 
 op :: (Num a, Stream s m Char) => ParsecT s u m (a -> a -> a)
-op =  (const (+) <$> char '+') <|>
-      (const (-) <$> char '-')
+op =  (const (+) <$> symbol "+") <|>
+      (const (-) <$> symbol "-")
 
-expr :: Stream s m Char => ParsecT s u m Int
+expr :: Stream s m Char => ParsecT s u m Double
 expr =  num `chainl1` op
 
 printParse str = print $ parse expr "" str
