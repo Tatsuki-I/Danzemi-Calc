a11c5ca52e57663dff42da3c23cca0506917b2fa
nishimorishunpei <12184082@g.matsuyama-u.ac.jp>
2019-07-10 14:36:25 +0900

    電卓

--- Python/nishimori/py.py
+++ Python/nishimori/py.py
@@ -1,1 +1,18 @@
-print('Hello')
+
+def x():
+    a = input('> ')
+    if '+' in a:
+        y = a.split('+')
+        return int(y[0]) + int(y[1])
+    elif '-' in a:
+        y = a.split('-')
+        return int(y[0]) - int(y[1])
+    elif '*' in a:
+        y = a.split('*')
+        return int(y[0]) * int(y[1])
+    elif '/' in a:
+        y = a.split('/')
+        return int(y[0]) / int(y[1])
+while True:
+    print(x())
+
